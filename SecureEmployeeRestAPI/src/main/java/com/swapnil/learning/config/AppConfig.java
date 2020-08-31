3c1555e1c43361b4397e404e77fa739ac8e60916
Swapnil Dangore <swapnil.dangore@tieto.com>
2020-08-31 21:11:12 +0530

    http to https Redirect

--- SecureEmployeeRestAPI/src/main/java/com/swapnil/learning/config/AppConfig.java
+++ SecureEmployeeRestAPI/src/main/java/com/swapnil/learning/config/AppConfig.java
@@ -3,7 +3,14 @@
  */
 package com.swapnil.learning.config;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,5 +24,40 @@ public class AppConfig {
 	@Bean
 	public ModelMapper modelMapper() {
 	    return new ModelMapper();
+	}
+	
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat =
+	      new TomcatServletWebServerFactory() {
+
+	        @Override
+	        protected void postProcessContext(Context context) {
+	          SecurityConstraint securityConstraint = new SecurityConstraint();
+	          securityConstraint.setUserConstraint("CONFIDENTIAL");
+	          SecurityCollection collection = new SecurityCollection();
+	          collection.addPattern("/*");
+	          securityConstraint.addCollection(collection);
+	          context.addConstraint(securityConstraint);
+	        }
+	      };
+	  tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+	  return tomcat;
+	}
+
+	@Value("${server.port.http}")
+	private int serverPortHttp;
+
+	@Value("${server.port}")
+	private int serverPortHttps;
+
+	private Connector createHttpConnector() {
+	  Connector connector =
+	      new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	  connector.setScheme("http");
+	  connector.setSecure(false);
+	  connector.setPort(serverPortHttp);
+	  connector.setRedirectPort(serverPortHttps);
+	  return connector;
 	}
 }
