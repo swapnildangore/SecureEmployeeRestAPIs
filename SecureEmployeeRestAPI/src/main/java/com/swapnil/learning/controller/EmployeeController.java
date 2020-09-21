06ddfc9832f015797445ef9efa120a3b227818c1
Swapnil Dangore <swapnil.dangore@tieto.com>
2020-09-21 15:11:45 +0530

    Cleanup

--- SecureEmployeeRestAPI/src/main/java/com/swapnil/learning/controller/EmployeeController.java
+++ SecureEmployeeRestAPI/src/main/java/com/swapnil/learning/controller/EmployeeController.java
@@ -12,7 +12,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,8 +29,6 @@ import com.swapnil.learning.repository.EmployeeRepository;
  * @author Swapnil Dangore
  */
 @RestController
-//@RequestMapping(path="/employees")
-@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
 
 	@Autowired
--- SecureEmployeeRestAPI/src/main/java/com/swapnil/learning/interceptor/LoggingInterceptor.java
+++ SecureEmployeeRestAPI/src/main/java/com/swapnil/learning/interceptor/LoggingInterceptor.java
@@ -8,6 +8,8 @@ import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,18 +20,20 @@ import org.springframework.web.servlet.ModelAndView;
  */
 public class LoggingInterceptor implements HandlerInterceptor{
 
+	Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		//Check for valid request header
-		String authCode = request.getHeader("SAMPLE_AUTH_TYPE");
+		/*String authCode = request.getHeader("SAMPLE_AUTH_TYPE");
 		
 		if(authCode==null || !"TEST".equals(authCode)) {
-			System.out.println("============= UNAUTHORISED REQUEST==========");
+			logger.debug("============= UNAUTHORISED REQUEST==========");
 			response.setStatus(HttpStatus.UNAUTHORIZED.value());
 			return false;
-		}
-		
+		}*/
+		logger.debug("================= Authorized request=========");
 		request.setAttribute("REQUEST_START_TIME", System.currentTimeMillis());
 		return true;
 	}
@@ -41,7 +45,7 @@ public class LoggingInterceptor implements HandlerInterceptor{
 		long startTime = (Long) request.getAttribute("REQUEST_START_TIME");
 		
 		//long requestStartTime = Long.parseLong(startTime!=null?startTime:"0");
-		System.out.println("Time taken to process = "+(System.currentTimeMillis()-startTime));
+		logger.debug("Time taken to process = "+(System.currentTimeMillis()-startTime));
 	}
 
 }
